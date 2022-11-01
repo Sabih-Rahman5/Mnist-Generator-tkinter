1e8124bd07b7361d501811ffacf61401d93cee90
Sabih Rahamn <sabih.562@gmail.com>
2022-11-01 23:52:33 +0500

    Clean up commit Cleaned up useless packadges Cleaned up variables and comments

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import torch
 from tkinter import *
-from tkinter import filedialog
-from tkinter.filedialog import askopenfile
-import matplotlib.pyplot as plt
-#from IPython.display import Image, display
 from torchvision.utils import save_image
-from PIL import ImageTk, Image
-import numpy as np
 
 
 import os
@@ -20,7 +14,7 @@ def denorm(x):
     return out.clamp(0, 1)
 
 
-Generator = torch.load('models\Generator.pth', map_location='cpu')
+Generator = torch.load('models/Generator.pth', map_location='cpu')
 
 
 window = Tk()
@@ -37,13 +31,10 @@ def generate_images():
     display_image(idx)
 
 
-
-
 def save_fake_images(num, index, rows):
     sample_vectors = torch.randn(num, 64)
     fake_images = Generator(sample_vectors)
     fake_images = fake_images.reshape(fake_images.size(0), 1, 28, 28)
-
     fake_fname = 'fake_images-{0:0=4d}.png'.format(index)
     print('saving:', fake_fname)
     save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=rows)
@@ -55,30 +46,8 @@ def display_image(idx):
     testlabel.config(image=img)
 
 
-
-
-'''y = Generator(torch.randn(2, 64))   #Generate 2 random vectors of latent_size(64)
-print(y.shape)
-
-gen_imgs = denorm(y.reshape((-1, 28, 28)).detach())
-plt.imshow(gen_imgs[0], cmap='gray')
-plt.show()
-
-
-plt.imshow(gen_imgs[1], cmap='gray')
-
-plt.show()'''
-
-
-# save_fake_images(1200, 0, 50)
-# pil_img = Image.open(os.path.join(sample_dir, 'fake_images-0000.png'))
-# im_array = np.asarray(pil_img)
-# plt.imshow(im_array)
-# plt.show()
-
-
-greeting = Label(window, text="Hello, Tkinter")
-greeting.pack()
+heading = Label(window, text="MNIST Handwritten Digits Generator", bg='darkblue')
+heading.pack()
 f1 = Frame(window, bg='royalblue1')
 f1.place(x=0, y=40, width=450, height=700)
 
@@ -104,8 +73,6 @@ e3.insert(END, '10')
 
 f2 = Frame(window, bg='green')
 f2.place(x=450, y=40, width=1000, height=700)
-#img = PhotoImage(file=r'samples\fake_images-{0:0=4d}.png'.format(0)).subsample(1, 1)
-
 img = PhotoImage(file=r'samples\Canvas.png').subsample(2, 3)
 
 testlabel = Label(f2, bg='pink', image=img)
@@ -116,20 +83,3 @@ b2 = Button(f1, text='Generate', bd=0, bg='royalblue1', fg='white', width=20, fo
             command=generate_images)
 b2.grid(row=2, column=2, pady=5, padx=50)
 window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
