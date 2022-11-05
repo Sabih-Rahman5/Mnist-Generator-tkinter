d102b8a63eddd6d6a3f173b65c5049cfe1d59229
Sabih Rahamn <sabih.562@gmail.com>
2022-11-05 21:12:03 +0500

    Interface Changes & Improvements

--- main.py
+++ main.py
@@ -35,51 +35,58 @@ def save_fake_images(num, index, rows):
     sample_vectors = torch.randn(num, 64)
     fake_images = Generator(sample_vectors)
     fake_images = fake_images.reshape(fake_images.size(0), 1, 28, 28)
-    fake_fname = 'fake_images-{0:0=4d}.png'.format(index)
-    print('saving:', fake_fname)
-    save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=rows)
+    fake_name = 'fake_images-{0:0=4d}.png'.format(index)
+    print('saving:', fake_name)
+    save_image(denorm(fake_images), os.path.join(sample_dir, fake_name), nrow=rows)
 
 
 def display_image(idx):
     global img
     img = PhotoImage(file=r'samples\fake_images-{0:0=4d}.png'.format(idx))
-    testlabel.config(image=img)
+    blank_label.config(image=img)
 
 
-heading = Label(window, text="MNIST Handwritten Digits Generator", bg='darkblue')
-heading.pack()
-f1 = Frame(window, bg='royalblue1')
-f1.place(x=0, y=40, width=450, height=700)
+h = Label(window, text='MNIST-Handwritten Digits Generator', bg='blue', fg='white', font=('calibre', 20, 'bold'))
+h.pack(side=TOP, fill=X)
 
-l1 = Label(f1, text='Enter No of images:', bg='light blue')
-l1.grid(row=2, column=0, pady=5, padx=3)
-e1 = Entry(f1, bd=0)
-e1.grid(row=2, column=1, pady=5)
+controls = Frame(window, bg='royalblue1')
+controls.pack(side=BOTTOM, fill=X)
+
+
+l1 = Label(controls, text='Enter No Of Images:', bg='royalblue1', height=2, font=('calibre', 9, 'bold'))
+l1.grid(row=0, column=0)
+e1 = Entry(controls, bd=0, width=10)
+e1.grid(row=0, column=1, padx=4)
 e1.insert(END, '10')
 
-l2 = Label(f1, text='Enter Index:', bg='light blue')
-l2.grid(row=3, column=0, pady=5, padx=3)
-e2 = Entry(f1, bd=0)
-e2.grid(row=3, column=1, pady=5)
+l2 = Label(controls, text='Enter Index:', bg='royalblue1', height=2, font=('calibre', 9, 'bold'))
+l2.grid(row=0, column=2, pady=5, padx=5)
+e2 = Entry(controls, bd=0, width=10)
+e2.grid(row=0, column=3, padx=5, pady=5)
 e2.insert(END, '0')
 
 
-l3 = Label(f1, text='Enter no. of columns', bg='light blue')
-l3.grid(row=4, column=0, pady=5, padx=3)
-e3 = Entry(f1, bd=0)
-e3.grid(row=4, column=1, pady=5)
+l3 = Label(controls, text='Enter No. Of Columns:', bg='royalblue1', height=2, font=('calibre', 9, 'bold'))
+l3.grid(row=0, column=4, pady=5, padx=5)
+e3 = Entry(controls, bd=0, width=10)
+e3.grid(row=0, column=5, padx=5, pady=5)
 e3.insert(END, '10')
 
 
 f2 = Frame(window, bg='green')
-f2.place(x=450, y=40, width=1000, height=700)
+f2.place(x=0, y=38, width=1366, height=568)
+
 img = PhotoImage(file=r'samples\Canvas.png').subsample(2, 3)
 
-testlabel = Label(f2, bg='pink', image=img)
-testlabel.place(relx=0.5, rely=0.5, anchor="center")
+blank_label = Label(f2, image=img)
+blank_label.place(relx=0.5, rely=0.5, anchor="center")
 
 
-b2 = Button(f1, text='Generate', bd=0, bg='royalblue1', fg='white', width=20, font=('calibre', 9, 'bold'),
+b1 = Button(controls, text='Generate', bd=0, bg='green', fg='white', font=('calibre', 9, 'bold'), width=20, height=5,
             command=generate_images)
-b2.grid(row=2, column=2, pady=5, padx=50)
+b1.grid(row=0, column=7, pady=5, padx=100)
+
+b2 = Button(controls, text='Exit', bd=0, bg='red', fg='white', command=window.destroy, width=20, height=5,
+            font=('calibre', 9, 'bold'))
+b2.grid(row=0, column=10, pady=5)
 window.mainloop()
